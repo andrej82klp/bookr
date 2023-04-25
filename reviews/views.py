f4b69e938c1bda42a646ca110a65a9c8e9a41e56
Andrej Neprokin <a.neprokin@gmail.com>
2023-04-25 10:45:49 +0100

    Initial commit

---
+++ reviews/views.py
@@ -0,0 +1,9 @@
+# from django.conf import settings
+from django.shortcuts import render
+
+def index(request):
+    # if settings.DEBUG:
+    #     import ipdb; ipdb.set_trace()
+    name = request.GET.get("name") or "unknown"
+    # return HttpResponse("Hey there, {}!".format(name))
+    return render(request, "base.html", { "name": name })
